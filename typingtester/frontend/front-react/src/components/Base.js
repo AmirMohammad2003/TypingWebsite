090840fcef4fcfa18cb7c30cb2b05656bd8680e8
AmirMohammad <amirmohammadghadimi8@gmail.com>
2022-01-22 00:30:30 +0330

    made the overall layout

--- typingtester/frontend/front-react/src/components/Base.js
+++ typingtester/frontend/front-react/src/components/Base.js
@@ -1,31 +1,35 @@
 import React from "react";
 import { Container } from "@mui/material";
+import { Outlet } from "react-router-dom";
+
+const IconButtonLink = ({ to, iconClass }) => {
+  return (
+    <a href={to} className="button-link">
+      <i className={`fas fa-${iconClass}`}></i>
+    </a>
+  );
+};
 
 export default () => {
   return (
     <>
-      <Container maxWidth="lg" style={{ backgroundColor: "white" }}>
+      <Container maxWidth="lg">
         <div className="wrapper-flex">
           <div className="center-content">
             <div id="topSide">
               <div id="logo">TypingTester</div>
               <div id="menu">
-                <a href="/" className="button-link">
-                  <i className="fas fa-keyboard"></i>
-                </a>
-                <a href="#" className="button-link">
-                  <i className="fas fa-crown"></i>
-                </a>
-                <a href="#" className="button-link">
-                  <i className="fas fa-info"></i>
-                </a>
-                <a href="/account" className="button-link">
-                  <i className="fas fa-user"></i>
-                </a>
+                <IconButtonLink to="/" iconClass="keyboard" />
+                <IconButtonLink to="#" iconClass="crown" />
+                <IconButtonLink to="#" iconClass="info" />
+                <IconButtonLink to="/account" iconClass="user" />
               </div>
             </div>
-            <div id="middleSide">item 2</div>
-            <div id="bottomSide">item 3</div>
+            <div id="middleSide">
+              <Outlet />
+            </div>
+
+            <div id="bottomSide">{/* todo */}</div>
           </div>
         </div>
       </Container>

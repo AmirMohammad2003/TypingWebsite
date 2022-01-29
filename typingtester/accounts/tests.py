7abb280a7c032de5b9fc521772f3850039767a99
AmirMohammad <amirmohammadghadimi8@gmail.com>
2022-01-29 13:54:58 +0330

    added more test cases

--- typingtester/accounts/tests.py
+++ typingtester/accounts/tests.py
@@ -165,7 +165,6 @@ class RegisterTestCases(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         _res = json.loads(response.content)
-        # print(_res['errors'])
         self.assertEqual(_res['success'], 'unknown')
         self.assertEqual(len(mail.outbox), 1)
         self.client.post(
@@ -175,3 +174,111 @@ class RegisterTestCases(TestCase):
         self.assertEqual(response.status_code, 200)
         _res = json.loads(response.content)
         self.assertEqual(_res['Authenticated'], 'false')
+
+
+class VerifyEmailTestCases(TestCase):
+
+    def setUp(self):
+        self.UserModel = get_user_model()
+        self.client = Client()
+
+    def test_verify_email_failure(self):
+        response = self.client.get(
+            '/auth/verify/asdf/adsfasdfasdfasdfasdfasdf/')
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['success'], 'false')
+
+
+class ForgotPasswordTestCases(TestCase):
+
+    def setUp(self):
+        self.UserModel = get_user_model()
+        self.client = Client()
+
+    def test_forgot_password_success(self):
+        self.UserModel.objects.create_user(
+            username='test', password='test', email='test@test.com'
+        )
+
+        response = self.client.post(
+            "/auth/reset/",
+            {'email': 'test@test.com'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['success'], 'true')
+        self.assertEqual(len(mail.outbox), 1)
+        url = mail.outbox[0].message().as_string().split('\n')[-1]
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        uidb64 = url.split('/')[-3]
+        token = url.split('/')[-2]
+        response = self.client.post(
+            '/auth/reset/confirm/',
+            {
+                'password1': 'testtesttest',
+                'password2': 'testtesttest',
+                'uidb64': uidb64,
+                'token': token
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['success'], 'true')
+        self.client.post(
+            '/auth/login/', {'username': 'test', 'password': 'testtesttest'}
+        )
+        response = self.client.post('/auth/check/')
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['Authenticated'], 'true')
+
+    def test_forgot_password_failure(self):
+        response = self.client.post(
+            "/auth/reset/",
+            {'email': 'dummy@test.com'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['success'], 'true')
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_forgot_password_logged_in(self):
+        self.user = self.UserModel.objects.create_user(
+            username='test', password='test', email='test@test.com'
+        )
+        self.user.is_email_verified = True
+        self.user.is_active = True
+        self.user.save()
+
+        self.client.post(
+            '/auth/login/', {'username': 'test', 'password': 'test'}
+        )
+
+        response = self.client.post(
+            "/auth/reset/",
+            {'email': 'test@test.com'}
+        )
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['success'], 'false')
+        self.assertEqual(len(mail.outbox), 0)
+        self.assertContains(response, 'logged in')
+
+    def test_forgot_password_confirm_bad_token(self):
+        response = self.client.post(
+            '/auth/reset/confirm/',
+            {
+                'password1': 'testtesttest',
+                'password2': 'testtesttest',
+                'uidb64': 'asldkfj',  # invalid data
+                'token': 'asldfjalskdfjalsjkdfl;a'  # invalid data
+            }
+        )
+        self.assertEqual(response.status_code, 200)
+        _res = json.loads(response.content)
+        self.assertEqual(_res['success'], 'false')
+        self.assertEqual(len(mail.outbox), 0)
